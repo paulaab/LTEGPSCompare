036128fd3cdfa80d8d29fe824ec13f1cd1cabcb7
Paula Bohorquez <paulabohorquez@Paula.local>
2017-05-24 15:06:33 +0200

    Executable finished.Last Version

--- parseText.py
+++ parseText.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
+import os
+myFiles = ['JSONGPSData.json','JSONLTEData.json','JSONNnewLTEData.json','DataJoined.json']
+for item in myFiles:
+    if os.path.isfile(item):
+        os.remove(item)
+
+
+
 print ("Starting...")
+
 import LTE_converter
 import GPS_converter
 import Comparer
